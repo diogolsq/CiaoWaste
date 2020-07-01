13a0307ac8979e180ff6608507b41c00d96a660d
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-01 15:17:21 -0300

    created validations trough TDDing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :address, presence: true
+  validates :email, uniqueness: true
+
+  has_many :orders, dependent: :destroy
+  has_many :restaurants
+  has_many :reviews, dependent: :destroy
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_initialize :init
+
+  def init
+    self.Admin ||= false
+    self.role ||= 'buyer'
+  end
+
 end
