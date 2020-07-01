926aadd9120b181623cb8a1778f53bcb8d9a06d1
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-01 14:15:55 -0300

    creating restaurant models and rspec files for my currently models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_164954) do
+ActiveRecord::Schema.define(version: 2020_07_01_171004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -46,5 +55,6 @@ ActiveRecord::Schema.define(version: 2020_07_01_164954) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
   add_foreign_key "restaurants", "users"
 end
