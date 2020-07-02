e6967ca909ae169ee73ccfacaf7419288d58f1e1
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-02 14:11:20 -0300

    rspec for restaurant done, such as validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,15 @@
-class Restaurant < ApplicationRecord
+  class Restaurant < ApplicationRecord
   belongs_to :user
+
+
+  validates :name, :telephone, :address, presence: true
+
+  after_initialize :init
+
+  def init
+    self.rating ||= 0
+
+  end
+
+
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Restaurant do
+  let(:diogo) { User.create(email:'diogolsq@hotmail.com', password:'123456', first_name:'Diogo', last_name: 'Queiroz', address: 'R.Francisco Otaviano n 23 apt 1003 bl 1 Rio de Janeiro' ) }
+  let(:carretao) { Restaurant.create(name:'carretao', address: 'General Osorio, Rio de Janeiro', telephone: '989893283', user_id: diogo.id) }
+
+  it "Restaurant is invalid without the address" do
+    restaurant = Restaurant.create(name:'carretao', user_id:diogo.id)
+    expect(restaurant).to_not be_valid
+  end
+
+  it "Restaurant is invalid without the name" do
+    restaurant = Restaurant.create(address:'General osorio, Rio de Janeiro', user_id:diogo.id)
+    expect(restaurant).to_not be_valid
+  end
+
+  it "Restaurant is invalid without the telephone" do
+    restaurant = Restaurant.create(name:'carretao', address:'General osorio, Rio de Janeiro', user_id:diogo.id)
+    expect(restaurant).to_not be_valid
+  end
+
+  it 'a new Restaurant should have a rating of zero ' do
+    expect(carretao.rating).to eq(0)
+  end
 end
