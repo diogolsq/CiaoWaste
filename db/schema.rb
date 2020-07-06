26f193990d4def8067c5c26ab1103253aa9db5d8
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-06 12:17:46 -0300

    adding name to the meal table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_03_181809) do
+ActiveRecord::Schema.define(version: 2020_07_06_151434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_07_03_181809) do
     t.datetime "datetime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["restaurant_id"], name: "index_meals_on_restaurant_id"
   end
 
