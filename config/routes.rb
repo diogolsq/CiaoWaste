49f6a22259f216c93a12be38fb35c2e6a99002ae
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-07-06 14:39:41 -0300

    created routes and restaurants controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  resources :meals do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+
+  resources :restaurants
+  resources :users, only: [ :show ]
+
+  resources :order_to_meals, only: [:create, :update, :destroy]
+  resources :orders, only: [ :show, :update ]
+
 end
